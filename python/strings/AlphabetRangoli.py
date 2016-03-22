08763b9ba1ff67c4a9b8e6f56269b1e47df72a92
Arjun Devarajan <arjund98@gmail.com>
2016-03-22 15:07:23 -0400

    March 22 challenge: Python, Strings, AR

--- python/strings/AlphabetRangoli.py
+++ python/strings/AlphabetRangoli.py
@@ -1,7 +1,17 @@
 n = int(raw_input())
 letters = 'abcdefghijklmnopqrstuvwxyz'
-for i in range(n):
-    print letters[n-i-1].center((n*4)-3,'-')
 
+def printing(finalList,addition):
+    finalList.reverse()
+    finalList.extend(addition)
+    line = '-'.join(finalList)
+    if (len(line)==(n*4-3)):
+        print line
+    else:
+        print line.center(n*4-3,'-')
+        
+for i in range(n):
+    printing(list(letters[n-i:n]),list(letters[n-i-1:n]))
+    
 for i in range(n-1):
-    print letters[i+1].center((n*4)-3,'-')
+    printing(list(letters[i+2:n]),list(letters[i+1:n]))
