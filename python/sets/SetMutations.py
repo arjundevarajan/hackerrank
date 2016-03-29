8e379a7771aa5658e4e5b0f1511cfa86a2b9b157
Arjun Devarajan <arjund98@gmail.com>
2016-03-28 23:45:48 -0400

    March 28 challenge extra improvement: Python, Sets, SM

--- python/sets/SetMutations.py
+++ python/sets/SetMutations.py
@@ -1,12 +1,4 @@
-n = input(); s = set(map(int,raw_input().split())); n = input()
-for i in range(n):
-    command = raw_input().split()
-    if command[0]=='intersection_update':
-        s &= set(map(int,raw_input().split()))
-    if command[0]=='update':
-        s |= set(map(int,raw_input().split()))
-    if command[0]=='symmetric_difference_update':
-        s ^= set(map(int,raw_input().split()))
-    if command[0]=='difference_update':
-        s -= set(map(int,raw_input().split()))
+n = input(); s = set(map(int,raw_input().split()))
+for i in range(input()):
+    exec 's.' + raw_input().split()[0] + '(set(map(int,raw_input().split())))'
 print sum(s)
