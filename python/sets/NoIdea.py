ae3ff5e327cbe0ecaae02e3f689207654b29ec86
Arjun Devarajan <arjund98@gmail.com>
2016-04-01 14:06:04 -0400

    April 1 challenge improvement: Python, Sets, NI

--- python/sets/NoIdea.py
+++ python/sets/NoIdea.py
@@ -1,8 +1,4 @@
-nums = map(int,raw_input().split()); n = nums[0]; m = nums[1]; array = map(int,raw_input().split()); Aset = set(map(int,raw_input().split())); Bset = set(map(int,raw_input().split()))
-happiness = 0
-for i in array:
-    if (i in Aset and i not in Bset):
-        happiness+=1
-    if (i in Bset and i not in Aset):
-        happiness-=1
-print happiness
+def read():
+    return raw_input().split()
+nums = map(int,read()); array = map(int,read()); Aset = set(map(int,read())); Bset = set(map(int,read()))
+print len([i for i in array if (i in Aset and i not in Bset)]) - len([i for i in array if (i in Bset and i not in Aset)])
